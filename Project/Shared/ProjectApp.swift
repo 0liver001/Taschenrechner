6eb16546824bc5a27189c3437a34d7ad83dd17ec
Oliver Kalemba / BBS2H20A <Oliver.Kalemba@edu.bib.de>
2021-08-23 08:37:45 +0200

    Projekt erstellung

---
+++ Project/Shared/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Shared
+//
+//  Created by Oliver Kalemba / BBS2H20A on 23.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
